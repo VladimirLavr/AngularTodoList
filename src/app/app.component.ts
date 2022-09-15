8b7c67f5423e8717d00c316fd123e45599c6e8b2
Vladimi <zapadpride@ukr.net>
2022-09-15 09:36:42 +0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-app';
+  todo: string = '';
+  todos: string[] = [];
+  disabled = false;
+
+
+  delItem(newItem: string) {
+    this.todos = this.todos.filter(items => items !== newItem);
+    console.log(this.todos);
+  }
+
+
+  changeInp(inpElem: string | any) {
+    if (inpElem.value.length >= 1) {
+      this.disabled = true
+    } else this.disabled = false
+  }
+
+  getValue(inpElem: string | any) {
+    if (inpElem.value) {
+      this.todo = inpElem.value
+      this.todos.push(this.todo)
+      inpElem.value = ''
+      this.disabled = false
+    }
+  }
 }
