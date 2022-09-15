5113d59ea6f7b5446b51d65d5642bea96b01bc06
VladimirLavr <81626942+VladimirLavr@users.noreply.github.com>
2022-09-15 21:55:44 +0300

    Revert "second commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,10 @@
-import {Component} from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  todo: string = '';
-  todos: string[] = [];
-  disabled = false;
-
-
-  delItem(newItem: string) {
-    this.todos = this.todos.filter(items => items !== newItem);
-    console.log(this.todos);
-  }
-
-
-  changeInp(inpElem: string | any) {
-    if (inpElem.value.length >= 1) {
-      this.disabled = true
-    } else this.disabled = false
-  }
-
-  getValue(inpElem: string | any) {
-    if (inpElem.value) {
-      this.todo = inpElem.value
-      this.todos.push(this.todo)
-      inpElem.value = ''
-      this.disabled = false
-    }
-  }
+  title = 'my-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {TodoItemComponent} from "./components/TodoItem/todoItem.component";
-import {FormsModule} from "@angular/forms";
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoItemComponent
-
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
